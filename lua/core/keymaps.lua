ac2f179499cbd9e146caadb1d8752a385b2873e2
DarthWixon <willdixon@protonmail.com>
2025-06-24 17:18:42 +0100

    add toggleterm

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -14,13 +14,13 @@ vim.keymap.set("n", "x", '"_x', opts)
 -- Buffers
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
-vim.keymap.set("n", "<leader>q", ":Bdelete!<CR>", opts)   -- close buffer
-vim.keymap.set("n", "<leader>x", "<cmd> enew <CR>", opts) -- new buffer
+vim.keymap.set("n", "<leader>q", ":Bdelete!<CR>", opts) -- close buffer
+vim.keymap.set("n", "<leader>n", "<cmd> enew <CR>", opts) -- new buffer
 
 -- Window management
-vim.keymap.set("n", "<leader>v", "<C-w>v", opts)      -- split window vertically
-vim.keymap.set("n", "<leader>h", "<C-w>s", opts)      -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=", opts)     -- make split windows equal width & height
+vim.keymap.set("n", "<leader>v", "<C-w>v", opts) -- split window vertically
+vim.keymap.set("n", "<leader>h", "<C-w>s", opts) -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=", opts) -- make split windows equal width & height
 vim.keymap.set("n", "<leader>xs", ":close<CR>", opts) -- close current split window
 
 -- Navigate between splits
@@ -35,3 +35,8 @@ vim.keymap.set("v", "p", '"_dP', opts)
 -- Explicitly yank to system clipboard (highlighted and entire row)
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- -- Terminal buffer commands
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", opts) -- exit input mode in terminal buffer
+vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>", opts) -- toggle floating terminal
+vim.keymap.set("n", "<leader>tv", ":ToggleTerm direction=vertical<CR>", opts) -- toggle vertical terminal
