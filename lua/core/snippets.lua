46f3c368a0f4f62f0978e13cf7eb681eb12ede77
DarthWixon <willdixon@protonmail.com>
2025-09-18 11:03:46 +0100

    auto update lazy plugins

--- lua/core/snippets.lua
+++ lua/core/snippets.lua
@@ -28,3 +28,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = highlight_group,
 	pattern = "*",
 })
+
+-- Automatically update packages on start
+local function augroup(name)
+	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = augroup("autoupdate"),
+	callback = function()
+		if require("lazy.status").has_updates then
+			require("lazy").update({ show = false })
+		end
+	end,
+})
